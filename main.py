6c02e679d93b77bb88ef4e784f12b1976daa4854
sopier <sopier@gmail.com>
2014-01-19 07:30:49 +0700

    code edit

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import os
 for fn in os.listdir('img'):
     filename, filext = os.path.splitext(fn)
     print filename
+
+from PIL import Image
+
+for fn in os.listdir('img'):
+    im = Image.open('img/' + fn)
+    print im.format, im.size, im.mode # format, dimension, mode
+    print os.stat('img/' + fn).st_size # filesize
